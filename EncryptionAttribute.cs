189118867a47184cdff3a2ea531e47891df94c68
Нарожный Владимир Викторович <narovova@hotmail.com>
2019-10-29 11:04:15 +0200

    added encrypt/decrypt  to attribute

--- EncryptionAttribute.cs
+++ EncryptionAttribute.cs
@@ -5,20 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Attribute
+namespace CreateAttribute
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct)]
     public class EncryptionAttribute : System.Attribute
     {
         public static string ClassName;
-        private object[] Parametrs;
+        public object[] Parametrs;
 
         public EncryptionAttribute(string className, params object[] parametrs)
         {
             ClassName = className;
             Parametrs = parametrs;
         }
-        
-        EncryptionAttribute MyClassType = (EncryptionAttribute)Activator.CreateInstance(Type.GetType(ClassName));
+
+        public IEncryptionService MyClassType = Activator.CreateInstance(Type.GetType(ClassName)) as IEncryptionService;
+
+       public void Encryption()
+        {
+            MyClassType.Encrypt(Parametrs[1].ToString(), Parametrs[2], Parametrs[3].ToString());
+        }
+
+        //public T Decryption<T>()
+        //{
+        //    return MyClassType.Decrypt(Parametrs[1].ToString(), Parametrs[3].ToString());
+        //}
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,20 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Attribute
+namespace CreateAttribute
 {
     class Program
     {
         public class TestAttribute
         {
-            [Encryption(className: "EncryptionService", parametrs: new object[] { "testfile.xml", test, "key" })]
-            public const string test = "test";
-           
+            [Encryption(className:"EncryptionService", parametrs: new object[] { "testfile.xml", test, "key" })]
+            public static string test = "test";
+
+            public void GetAttribute(Type t)
+            {
+                EncryptionAttribute MyAttribute = (EncryptionAttribute)Attribute.GetCustomAttribute(t, typeof(EncryptionAttribute));
+
+                if (MyAttribute == null)
+                {
+                    Console.WriteLine("Attribute not fond");
+                }
+
+                MyAttribute.Encryption();
+            }
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
-         
+            TestAttribute test = new TestAttribute();
+            test.GetAttribute(typeof(Program));
+
         }
     }
 }
